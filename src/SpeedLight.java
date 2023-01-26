6012ad5a0d5595756be59973e8a1063b29981a0d
Peyton Holland <pholland3@ggc.edu>
2023-01-26 08:22:43 -0500

    added commented line.

---
+++ src/SpeedLight.java
@@ -0,0 +1,20 @@
+/**
+ * @Class Name: SpeedLight
+ * @Author: Peyton Holland
+ * @Version: 1.0
+ * @Date: 26 January 2023
+ * Description: A program designed to calculate the speed of light
+ */
+
+
+
+
+
+
+
+public class SpeedLight {
+    public static void main(String[]args){
+        //complete here
+
+    }
+}
