4573425998a7d60a87c08f4b5fec021259e468a9
Peyton Holland <pholland3@ggc.edu>
2023-01-26 18:42:23 -0500

    Edited SpeedLight.java to complete program per instructions

--- src/SpeedLight.java
+++ src/SpeedLight.java
@@ -3,18 +3,19 @@
  * @Author: Peyton Holland
  * @Version: 1.0
  * @Date: 26 January 2023
- * Description: A program designed to calculate the speed of light
+ * Description: A program designed to calculate the distance between a lightning strike and the observer, given
+ * the speed of light at 340 meters per second, and the observer hearing the sound of thunder 5 seconds later.
  */
 
-
-
-
-
-
-
 public class SpeedLight {
-    public static void main(String[]args){
-        //edited
-
+    public static void main(String[]args)
+    {
+        int speedOfSound = 340;
+        int soundOfThunder = 5;
+        double distance = speedOfSound / soundOfThunder;
+        System.out.println("The observer sees a flash of lightning, followed by the sound of thunder.");
+        System.out.println("The observer hears the sound of thunder " + soundOfThunder + " seconds after the lightning.");
+        System.out.println("Given that the speed of sound is " + speedOfSound + " meters per second,");
+        System.out.println("the observer is " + distance + " meters away from the lightning strike.");
     }
 }
