957dfae3dbbbd2cd9616a51683363596b027f3c7
Peyton Holland <pholland3@ggc.edu>
2023-01-26 08:30:12 -0500

    add Chocolate.java

---
+++ src/Chocolate.java
@@ -0,0 +1,12 @@
+/**
+ * @Class: Chocolate
+ * @Author: Peyton Holland
+ * @Version 1.0
+ * @Course: ITEC 2140 Section 05 Spring 2023
+ * @Written: 26 January 2023
+ *
+ * Description - Chocolate
+ */
+
+public class Chocolate {
+}
